c0ada4f4748939dfc3b20e6e4653405c56a54a5c
azarath1 <ar2oww@gmail.com>
2022-01-19 11:28:03 +0100

    feat: add response userdata

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const port = 4000;
-const cors = require('cors');
-// const bodyParser = require('body-parser');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(bodyParser.json());
 app.use(cors());
 
 app.post('/login', async (req, res) => {
     let reqbody = req.body;
-    if (reqbody){
+    if (reqbody) {
         return res.status(200).send({
-            reqbody
-        });    
+            success: true,
+            message: 'success',
+            username: reqbody.email.split("@")[0],
+            userid: 1
+        });
     }
 });
 
