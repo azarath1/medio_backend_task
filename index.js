672e10ff715848cc9ef4e959979a36528698beb9
azarath1 <ar2oww@gmail.com>
2022-01-19 09:02:32 +0100

    feat: basic post request endpoint for login

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
 const port = 4000;
+const cors = require('cors');
+// const bodyParser = require('body-parser');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/login', async (req, res) => {
+    let reqbody = req.body;
+    if (reqbody){
+        return res.status(200).send({
+            reqbody
+        });    
+    }
+});
+
 
 app.listen(port, () => {
     console.log(`listening at http://localhost:${port}`)
